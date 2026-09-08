3c613015f54c7d8ea747379d65b90c918d56813f
SirRayan <RayanLeex@gmail.com>
2026-09-08 15:46:12 +0300

    fix GitHub Pages deployment

--- lib/shared.ts
+++ lib/shared.ts
@@ -5,7 +5,7 @@ export const docsContentRoute = "/llms.mdx/docs";
 
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
-  user: "overextended",
-  repo: "docs",
+  user: "SirRayan",
+  repo: "SirRayan.github.io",
   branch: "main",
 };
