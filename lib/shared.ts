06374e2cbf58788b615977bbe01801fbe708b54a
SirRayan <RayanLeex@gmail.com>
2026-09-09 03:16:23 +0300

    Update shared.ts

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,4 +1,4 @@
-export const appName = "Overextended";
+export const appName = "7MStudio";
 export const docsRoute = "/docs";
 export const docsImageRoute = "/og/docs";
 export const docsContentRoute = "/llms.mdx/docs";
