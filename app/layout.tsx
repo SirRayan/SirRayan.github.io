297738f79aaf01a48e034f3560896ba179c03d3d
SirRayan <RayanLeex@gmail.com>
2026-09-09 03:21:27 +0300

    Update package name and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import { Inter } from "next/font/google";
 import { Provider } from "@/components/provider";
+import type { Metadata } from "next";
 import "./global.css";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/logo.webp",
+  },
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
