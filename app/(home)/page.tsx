559517d5a54b3c905b7faab1039a5bafdfe1711d
SirRayan <RayanLeex@gmail.com>
2026-09-09 03:16:31 +0300

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1 prose items-center">
-      <h1 className="text-2xl font-bold">Welcome to Overextended</h1>
+      <h1 className="text-2xl font-bold">Welcome to 7MStudio</h1>
 
       <a href="https://ko-fi.com/thelindat">
         <img src="/static/kofi.png" className="w-40" />
